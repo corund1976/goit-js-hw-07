a0ee94e379ca77a26502a97f60461f81385f3d6e
Sergii Budak <corund1976@gmail.com>
2021-09-27 18:26:31 +0300

    fin

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const divGallery = document.querySelector('.gallery');
+
+galleryItems.map(image => {
+    
+    let markUp = `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${image.original}">
+            <img
+                class="gallery__image"
+                src="${image.preview}"
+                data-source="${image.original}"
+                alt="${image.description}"
+            />
+        </a>
+    </div>
+    `;
+
+    divGallery.insertAdjacentHTML('beforeend', markUp);
+});
+
+divGallery.addEventListener('click', onClick);
+
+function onClick(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+
+    const instance = basicLightbox.create(`
+        <img src="${event.target.dataset.source}">
+    `)
+
+    instance.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const divGallery = document.querySelector('.gallery');
+
+galleryItems.map(image => {
+
+    let markUp = `
+        <a class="gallery__item" href="${image.original}">
+            <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+        </a>
+    `;
+
+    divGallery.insertAdjacentHTML('beforeend', markUp);
+});
+
+
+divGallery.addEventListener('click', onClick);
+
+function onClick(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+
+    const lightbox = new SimpleLightbox('.gallery a', {
+        captions: true,
+        captionData: 'alt',
+        captionDelay: 250
+    });
+}
